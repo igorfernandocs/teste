49a156a354bc8f017f58acacf4b1c01dc1166e1c
Igor <igu_fernando@hotmail.com>
2024-03-21 20:43:35 -0300

    fix composer

--- .platform/hooks/postdeploy/00-permission.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x .platform/hooks/postdeploy/02-composer_and_permission.sh
-touch /var/www/html/igor.txt
--- .platform/hooks/postdeploy/02-composer_and_permission.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /var/www/html/
-composer install --no-interaction > /var/log/composer.log
-php artisan migrate --seed
-php artisan clear-compiled
-php artisan config:cache
-php artisan cache:clear
-php artisan route:cache
-php artisan view:clear
