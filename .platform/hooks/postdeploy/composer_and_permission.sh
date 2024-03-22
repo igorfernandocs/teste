d68fc6e83274d97ff21957676321d3a6fe82fda6
Igor <igu_fernando@hotmail.com>
2024-03-21 22:08:05 -0300

    fix composer

--- .platform/hooks/postdeploy/composer_and_permission.sh
+++ .platform/hooks/postdeploy/composer_and_permission.sh
@@ -2,7 +2,7 @@
 cd /var/www/html/
 /usr/bin/composer.phar self-update
 /usr/bin/composer.phar install --no-interaction > /var/log/composer.log
-php artisan migrate --seed
+php artisan migrate --seed > /var/log/migrate.log
 php artisan clear-compiled
 php artisan config:cache
 php artisan cache:clear
