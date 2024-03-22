41766f42fd6df9e66763887fc3d8ad72108d93ef
Igor <igu_fernando@hotmail.com>
2024-03-21 22:50:20 -0300

    fix composer

--- .platform/hooks/postdeploy/composer_and_permission.sh
+++ .platform/hooks/postdeploy/composer_and_permission.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd /var/www/html/
+CURRENT_DATE=$(date +"%Y-%m-%d %T")
 /usr/bin/composer.phar self-update
-/usr/bin/composer.phar install --no-interaction > /var/log/composer.log
-php artisan migrate --seed > /var/log/migrate.log
+/usr/bin/composer.phar install --no-interaction > /var/log/composer.log 2>&1
+echo "Composer install executado em: $CURRENT_DATE" >> /var/log/composer.log
+php artisan migrate --seed > /var/log/migrate.log 2>&1
+echo "Migrações e seed executados em: $CURRENT_DATE" >> /var/log/migrate.log
 php artisan clear-compiled
 php artisan config:cache
 php artisan cache:clear
