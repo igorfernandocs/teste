878e93637b7d7e1a4b537ad320cac0684f253799
Igor <igu_fernando@hotmail.com>
2024-03-25 17:01:54 -0300

    yarn fix

--- .platform/hooks/postdeploy/composer_and_permission.sh
+++ .platform/hooks/postdeploy/composer_and_permission.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd /var/www/html/
-curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
-yum install yarn
-npm install -g create-vite
-npm install vite
 COMPOSER_ALLOW_SUPERUSER=1
 CURRENT_DATE=$(date +"%Y-%m-%d %T")
 /usr/bin/composer.phar self-update
@@ -16,5 +12,9 @@ php artisan config:cache
 php artisan cache:clear
 php artisan route:cache
 php artisan view:clear
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
+yum install yarn
+npm install -g create-vite
+npm install vite
 yarn install /var/log/yarn.log 2>&1
 yarn build /var/log/yarn.log 2>&1
