9335e6d14170267e8549d08c9ba61963484251ad
Igor <igu_fernando@hotmail.com>
2024-03-25 17:15:35 -0300

    yarn fix

--- .platform/hooks/postdeploy/composer_and_permission.sh
+++ .platform/hooks/postdeploy/composer_and_permission.sh
@@ -7,14 +7,3 @@ CURRENT_DATE=$(date +"%Y-%m-%d %T")
 echo "Composer install executado em: $CURRENT_DATE" >> /var/log/composer.log
 php artisan migrate --seed > /var/log/migrate.log 2>&1
 echo "Migrações e seed executados em: $CURRENT_DATE" >> /var/log/migrate.log
-php artisan clear-compiled
-php artisan config:cache
-php artisan cache:clear
-php artisan route:cache
-php artisan view:clear
-curl -sL https://dl.yarnpkg.com/rpm/yarn.repo -o /etc/yum.repos.d/yarn.repo
-yum install yarn
-npm install -g create-vite
-npm install vite
-yarn install /var/log/yarn.log 2>&1
-yarn build /var/log/yarn.log 2>&1
